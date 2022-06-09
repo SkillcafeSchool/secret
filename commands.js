2303cbeb714c3d2acefe324c7e18fe12dee65c28
Skillcafe School <31744635+SkillcafeSchool@users.noreply.github.com>
2022-06-09 14:17:36 +0430

    Add files via upload

---
+++ commands.js
@@ -0,0 +1,119 @@
+import { getRPSChoices } from './game.js';
+import { capitalize, DiscordRequest } from './utils.js';
+
+export async function HasGuildCommands(appId, guildId, commands) {
+  if (guildId === '' || appId === '') return;
+
+  commands.forEach((c) => HasGuildCommand(appId, guildId, c));
+}
+
+// Checks for a command
+async function HasGuildCommand(appId, guildId, command) {
+  // API endpoint to get and post guild commands
+  const endpoint = `applications/${appId}/guilds/${guildId}/commands`;
+
+  try {
+    const res = await DiscordRequest(endpoint, { method: 'GET' });
+    const data = await res.json();
+
+    if (data) {
+      const installedNames = data.map((c) => c['name']);
+      // This is just matching on the name, so it's not good for updates
+      if (!installedNames.includes(command['name'])) {
+        console.log(`Installing "${command['name']}"`);
+        InstallGuildCommand(appId, guildId, command);
+      } else {
+        console.log(`"${command['name']}" command already installed`);
+      }
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+// Installs a command
+export async function InstallGuildCommand(appId, guildId, command) {
+  // API endpoint to get and post guild commands
+  const endpoint = `applications/${appId}/guilds/${guildId}/commands`;
+  // install command
+  try {
+    await DiscordRequest(endpoint, { method: 'POST', body: command });
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+// Get the game choices from game.js
+function createCommandChoices() {
+  const choices = getRPSChoices();
+  const commandChoices = [];
+
+  for (let choice of choices) {
+    commandChoices.push({
+      name: capitalize(choice),
+      value: choice.toLowerCase(),
+    });
+  }
+
+  return commandChoices;
+}
+
+// Simple test command
+export const TEST_COMMAND = {
+  name: 'test',
+  description: 'Basic guild command',
+  type: 1,
+};
+
+// Command containing options
+export const CHALLENGE_COMMAND = {
+  name: 'challenge',
+  description: 'Challenge to a match of rock paper scissors',
+  options: [
+    {
+      type: 3,
+      name: 'object',
+      description: 'Pick your object',
+      required: true,
+      choices: createCommandChoices(),
+    },
+  ],
+  type: 1,
+};
+
+// Bot
+
+// "challenge" guild command
+if (name === 'challenge' && id) {
+    const userId = req.body.member.user.id;
+    // User's object choice
+    const objectName = req.body.data.options[0].value;
+
+    // Create active game using message ID as the game ID
+    activeGames[id] = {
+        id: userId,
+        objectName,
+    };
+
+    return res.send({
+    type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+    data: {
+        // Fetches a random emoji to send from a helper function
+        content: `Rock papers scissors challenge from <@${userId}>`,
+        components: [
+        {
+            type: MessageComponentTypes.ACTION_ROW,
+            components: [
+            {
+                type: MessageComponentTypes.BUTTON,
+                // Append the game ID to use later on
+                custom_id: `accept_button_${req.body.id}`,
+                label: 'Accept',
+                style: ButtonStyleTypes.PRIMARY,
+            },
+            ],
+        },
+        ],
+    },
+    });
+}
